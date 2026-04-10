2cd838acf5055f02263f210238e213364048cfb5
Harry Dehler <harrydehler@gmail.com>
2026-04-10 12:00:18 -0400

    remove dashboard-main

--- dashboard-main/server/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/hdehler/slide-dashboard/backend
-
-go 1.24.1
-
-require (
-	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.10.9
-)
--- dashboard-main/web/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders dashboard", () => {
-  render(<App />);
-  const logoElement = screen.getByAltText("Slide");
-  expect(logoElement).toBeInTheDocument();
-});
